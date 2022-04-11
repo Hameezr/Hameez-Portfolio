a5b273c82da1a0594f88cec68a03a6f25a4b0d6e
Hameez Rizwan <hameexr@gmail.com>
2022-04-11 23:56:10 +0500

    skills updated

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,12 +1,15 @@
  export const skillsData = [
     'HTML',
-    'Fastify',
-    'Blender',
     'Figma',
     'Javascript',
     'CSS',
     'React',
     'Django',
+    'Python',
+    'Next JS',
+    'Adobe XD',
+    'Typescript',
+    'Postgresql',
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.

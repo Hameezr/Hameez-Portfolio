cd5d1ad35894e89d967954f90dbdfe8dd17b8faa
Hameez Rizwan <hameexr@gmail.com>
2022-04-12 10:13:12 +0500

    removed comment

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -82,19 +82,3 @@ export const projectsData = [
         image: eight
     },
 ]
-
-
-// Do not remove any fields.
-// Leave it blank instead as shown below
-
-/* 
-{
-    id: 1,
-    projectName: 'Car Pooling System',
-    projectDesc: '',
-    tags: ['Flutter', 'React'],
-    code: '',
-    demo: '',
-    image: ''
-}, 
-*/

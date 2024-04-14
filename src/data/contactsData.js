da54d682f98d80447e9e308f65ca495502e4efb0
Hameez Rizwan <hameexr@gmail.com>
2024-04-14 20:35:14 +0500

    Added CT to experiences

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,5 +1,5 @@
 export const contactsData = {
     email: 'hameexr@gmail.com',
     phone: '+923444225504',
-    address: '392-P Block, Dha, Lahore, Pakistan',
+    address: 'Lahore, Pakistan',
 }
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,10 +1,28 @@
 export const experienceData = [
     {
         id: 1,
+        company: 'CarbonTeq',
+        jobtitle: 'Full Stack Developer',
+        startYear: 'July, 2023',
+        endYear: 'Present',
+        details: [`Led the backend team in engineering optimal solutions for a fintech product, with focus on optimizing system architecture and reducing technical debt. This involved implementing best coding practices and embracing advanced technologies, resulting in efficient, scalable, and robust backend services.`,
+    'Developed a Fintech B2B SaaS product using NestJS, TypeORM, and PostgreSQL, focusing on robust performance and security.',
+    'Implemented Domain-Driven Design (DDD) and Hexagonal Architecture principles to enhance scalability, maintainability, and consistency of the codebase.',
+    'Applied DDD techniques to accurately interpret client requirements, prioritizing business objectives and use cases to facilitate business scaling and enhance the UX.',
+    `Implemented Stripe and ACHQ for sophisticated payment gateway processing, managing complex payment infrastructures including recurring, subscription, and custom payment flows involving 4 unique entities.`,
+    `Conducted in-depth PR reviews to maintain high standards of code quality.`,
+    `Assisted deployment processes using GCP, Docker, Kubernetes, and Helm charts, ensuring efficient and reliable deployment cycles.`,
+    `Developed a comprehensive logging strategy, ensuring high system availability and real-time problem diagnosis.`,
+    `Implemented rigorous security protocols safeguarding sensitive financial data.`,
+    `Played a pivotal role in the decision-making process for technology stacks and tool selection, aligning with project requirements and long-term objectives.`,
+    ]
+    },
+    {
+        id: 2,
         company: 'BAT (British American Tobacco)',
         jobtitle: 'Full Stack Developer',
         startYear: 'August, 2022',
-        endYear: 'Present',
+        endYear: 'July, 2023',
         details: [`Developing an AI based OCR web platform to automate document reviews and validation process.`,
     'Developing UI/UX features using React Typescript, Python, HTML and CSS.',
     'Developing backend APIs using Python, with integration to Azure CosmosDB, Azure Storage, and GCP Vision API. ',
@@ -13,7 +31,7 @@ export const experienceData = [
     ]
     },
     {
-        id: 2,
+        id: 3,
         company: 'Carteblanche Innovation Integrated',
         jobtitle: 'Full Stack Engineer',
         startYear: 'June, 2020',
@@ -26,7 +44,7 @@ export const experienceData = [
     ]
     },
     {
-        id: 3,
+        id: 4,
         company: '3 Musafir',
         jobtitle: 'ReactJS Developer',
         startYear: 'Sept, 2019',
@@ -36,7 +54,7 @@ export const experienceData = [
         'Led UX research with a team of 3 people, to create user stories, non-functional requirements, and acceptance criteria, and prepared test cases.'],
     },
     {
-        id: 4,
+        id: 5,
         company: 'AIESEC, Turkey',
         jobtitle: 'Global Exchange Participant',
         startYear: 'June, 2019',

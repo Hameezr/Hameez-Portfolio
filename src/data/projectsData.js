da275fc7fce7c9b468f43b71d8c65be58f0eb634
Hameez Rizwan <hameexr@gmail.com>
2024-08-30 03:30:46 +0500

    added project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,3 +1,4 @@
+import twin from '../assets/png/twin.png'
 import one from '../assets/svg/projects/one.svg'
 import two from '../assets/svg/projects/two.svg'
 import three from '../assets/svg/projects/three.svg'
@@ -10,6 +11,15 @@ import seven from '../assets/svg/projects/seven.svg'
 export const projectsData = [
     {
         id: 1,
+        projectName: 'Fintech Loan Financing App',
+        projectDesc: 'I developed a Fintech app that offers specialized loan financing solutions for both individuals and businesses. The app includes Personal Premium Financing, which allows individuals to manage their insurance premium costs by spreading payments over time. Additionally, I incorporated Commercial Premium Financing to help businesses optimize their cash flow by distributing insurance premium payments over a specified period.',
+        tags: ['Nest', 'Node', 'GCP', 'React', 'Next'],
+        code: 'https://twinapp.com/',
+        demo: 'https://twinapp.com/',
+        image: twin
+    },
+    {
+        id: 2,
         projectName: 'Applicant Tracking System',
         projectDesc: 'An ATS that is one hiring software that enables to attract quality applicants & evaluate them collaboratively so the users can find the right fit for their team.',
         tags: ['React', 'Python', 'Django', 'Material Ui', 'Redux/Saga'],
@@ -18,7 +28,7 @@ export const projectsData = [
         image: one
     },
     {
-        id: 2,
+        id: 3,
         projectName: 'Profile Portfolio',
         projectDesc: 'This project was to build a portfolio using Javascript for a client, client\'s information is kept confidential hence I\'ve added my own information',
         tags: ['Javascript', 'CSS', 'HTML'],
@@ -27,7 +37,7 @@ export const projectsData = [
         image: two
     },  
     {
-        id: 3,
+        id: 4,
         projectName: 'Material-Ui OpenSource contribution',
         projectDesc: 'Developed a component in material-ui Datagrid as an open source contribution with all the benchmarks',
         tags: ['React', 'Javascript'],
@@ -36,7 +46,7 @@ export const projectsData = [
         image: four
     },
     {
-        id: 4,
+        id: 5,
         projectName: 'Pacman + Bomberman',
         projectDesc: 'A simple Pacman game with a twist, it now has features of bomberman as well!',
         tags: ['C++', 'C', 'HTML', 'CSS'],
@@ -45,7 +55,7 @@ export const projectsData = [
         image: seven
     },
     {
-        id: 5,
+        id: 6,
         projectName: 'E-Commerce App',
         projectDesc: 'A Simple E-commerce application',
         tags: ['React Native', 'Firebase'],
@@ -54,7 +64,7 @@ export const projectsData = [
         image: five
     },
     {
-        id: 6,
+        id: 7,
         projectName: 'Cookie Crush',
         projectDesc: 'A game coded in C++ similar to candy crush',
         tags: ['C++, C', 'HTML', 'CSS'],
@@ -63,7 +73,7 @@ export const projectsData = [
         image: six
     },
     {
-        id: 7,
+        id: 8,
         projectName: 'Learning Management System',
         projectDesc: 'This project aims to build a student management system that consolidates all information of a student and his institution',
         tags: ['Java', 'MySQL'],
